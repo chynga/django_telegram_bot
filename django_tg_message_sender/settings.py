8a7e527f4de5de2aaa69f9e96375b88ebe604c81
Shynggys <chynga2002@gmail.com>
2022-12-30 14:07:22 +0600

    heroku

--- django_tg_message_sender/settings.py
+++ django_tg_message_sender/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'base',
